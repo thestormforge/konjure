99c34494ce31bbd71cf27f0e8f02500f8e58538b
Jeremy Gustie <jeremy@stormforge.io>
2021-02-22 16:51:36 -0500

    Add comments to the API types

--- pkg/api/core/v1beta2/types.go
+++ pkg/api/core/v1beta2/types.go
@@ -21,118 +21,187 @@ import (
 	"sigs.k8s.io/kustomize/kyaml/yaml"
 )
 
+// Resource is used to expand a list of URL-like specifications into other Konjure resources.
 type Resource struct {
 	yaml.ResourceMeta `json:",inline" yaml:",inline"`
 
+	// The list of URL-like specifications to convert into Konjure resources.
 	Resources []string `json:"resources" yaml:"resources"`
 }
 
+// HelmExecutor contains additional configuration for the Helm binary.
 type HelmExecutor struct {
-	Bin             string `json:"bin,omitempty" yaml:"bin,omitempty"`
+	// The path to the Helm binary. Defaults to "helm".
+	Bin string `json:"bin,omitempty" yaml:"bin,omitempty"`
+	// The path to the Helm repository cache. Corresponds to the `helm --repository-cache` option.
 	RepositoryCache string `json:"repositoryCache,omitempty" yaml:"repositoryCache,omitempty"`
 }
 
+// HelmValue specifies a value or value file for configuring a Helm chart.
 type HelmValue struct {
-	File        string `json:"file,omitempty" yaml:"file,omitempty"`
-	Name        string `json:"name,omitempty" yaml:"name,omitempty"`
-	Value       string `json:"value,omitempty" yaml:"value,omitempty"`
-	ForceString bool   `json:"forceString,omitempty" yaml:"forceString,omitempty"` // TODO Eliminate and use IntOrString?
-	LoadFile    bool   `json:"loadFile,omitempty" yaml:"loadFile,omitempty"`
+	// Path to a values.yaml file.
+	File string `json:"file,omitempty" yaml:"file,omitempty"`
+	// Name of an individual name/value to set.
+	Name string `json:"name,omitempty" yaml:"name,omitempty"`
+	// Value of an individual name/value to set.
+	Value string `json:"value,omitempty" yaml:"value,omitempty"`
+	// Flag indicating that numeric like value should be quoted as strings (e.g. for environment variables).
+	ForceString bool `json:"forceString,omitempty" yaml:"forceString,omitempty"` // TODO Eliminate and use IntOrString?
+	// Treat value as a file and load the contents in place of the actual value.
+	LoadFile bool `json:"loadFile,omitempty" yaml:"loadFile,omitempty"`
 }
 
+// Helm is used to expand a Helm chart locally (using `helm template`).
 type Helm struct {
 	yaml.ResourceMeta `json:",inline" yaml:",inline"`
 
-	Helm             HelmExecutor `json:"helm,omitempty" yaml:"helm,omitempty"`
-	ReleaseName      string       `json:"releaseName,omitempty" yaml:"releaseName,omitempty"`
-	ReleaseNamespace string       `json:"releaseNamespace,omitempty" yaml:"releaseNamespace,omitempty"`
-	Chart            string       `json:"chart" yaml:"chart"`
-	Version          string       `json:"version,omitempty" yaml:"version,omitempty"`
-	Repository       string       `json:"repo" yaml:"repo"`
-	Values           []HelmValue  `json:"values,omitempty" yaml:"values,omitempty"`
-	IncludeTests     bool         `json:"includeTests,omitempty" yaml:"includeTests,omitempty"`
+	// Additional Helm configuration.
+	Helm HelmExecutor `json:"helm,omitempty" yaml:"helm,omitempty"`
+	// The release name to use when rendering the chart templates.
+	ReleaseName string `json:"releaseName,omitempty" yaml:"releaseName,omitempty"`
+	// The namespace to use when rendering the chart templates (this is particularly important for charts that
+	// may produce resources in multiple namespaces).
+	ReleaseNamespace string `json:"releaseNamespace,omitempty" yaml:"releaseNamespace,omitempty"`
+	// The chart name to inflate.
+	Chart string `json:"chart" yaml:"chart"`
+	// The specific version of the chart to use (defaults to the latest release).
+	Version string `json:"version,omitempty" yaml:"version,omitempty"`
+	// The repository URL to get the chart from.
+	Repository string `json:"repo" yaml:"repo"`
+	// The values used to configure the chart.
+	Values []HelmValue `json:"values,omitempty" yaml:"values,omitempty"`
+	// Flag to filter out tests from the results.
+	IncludeTests bool `json:"includeTests,omitempty" yaml:"includeTests,omitempty"`
 }
 
+// JsonnetParameter specifies inputs to a Jsonnet program.
 type JsonnetParameter struct {
-	Name       string `json:"name,omitempty" yaml:"name,omitempty"`
-	String     string `json:"string,omitempty" yaml:"string,omitempty"`
+	// The name of the parameter.
+	Name string `json:"name,omitempty" yaml:"name,omitempty"`
+	// The string value of the parameter.
+	String string `json:"string,omitempty" yaml:"string,omitempty"`
+	// The file name containing a string parameter value.
 	StringFile string `json:"stringFile,omitempty" yaml:"stringFile,omitempty"`
-	Code       string `json:"code,omitempty" yaml:"code,omitempty"`
-	CodeFile   string `json:"codeFile,omitempty" yaml:"codeFile,omitempty"`
+	// Code to include.
+	Code string `json:"code,omitempty" yaml:"code,omitempty"`
+	// The file name containing code to include.
+	CodeFile string `json:"codeFile,omitempty" yaml:"codeFile,omitempty"`
 }
 
+// Jsonnet is used to expand programmatically constructed resources.
 type Jsonnet struct {
 	yaml.ResourceMeta `json:",inline" yaml:",inline"`
 
-	Filename          string             `json:"filename" yaml:"filename"`
-	Code              string             `json:"exec" yaml:"exec"`
-	JsonnetPath       []string           `json:"jpath" yaml:"jpath"`
-	ExternalVariables []JsonnetParameter `json:"extVar" yaml:"extVar"`
-	TopLevelArguments []JsonnetParameter `json:"topLevelArg" yaml:"topLevelArg"`
+	// The Jsonnet file to evaluate.
+	Filename string `json:"filename,omitempty" yaml:"filename,omitempty"`
+	// An anonymous code snippet to evaluate.
+	Code string `json:"exec,omitempty" yaml:"exec,omitempty"`
+	// Additional directories to consider when importing additional Jsonnet code.
+	JsonnetPath []string `json:"jpath,omitempty" yaml:"jpath,omitempty"`
+	// The list of external variables to evaluate against.
+	ExternalVariables []JsonnetParameter `json:"extVar,omitempty" yaml:"extVar,omitempty"`
+	// The list of top level arguments to evaluate against.
+	TopLevelArguments []JsonnetParameter `json:"topLevelArg,omitempty" yaml:"topLevelArg,omitempty"`
 
-	JsonnetBundlerPackageHome string `json:"jbPkgHome" yaml:"jbPkgHome"`
-	JsonnetBundlerRefresh     bool   `json:"jbRefresh" yaml:"jbRefresh"`
+	// Explicit directory to use fo Jsonnet Bundler support (defaults to "vendor" if "jsonnetfile.json" is present).
+	JsonnetBundlerPackageHome string `json:"jbPkgHome,omitempty" yaml:"jbPkgHome,omitempty"`
+	// Flag to force a Bundler refresh, even if the package home directory is already present.
+	JsonnetBundlerRefresh bool `json:"jbRefresh,omitempty" yaml:"jbRefresh,omitempty"`
 }
 
+// Kubernetes is used to expand resources found in a Kubernetes cluster.
 type Kubernetes struct {
 	yaml.ResourceMeta `json:",inline" yaml:",inline"`
 
-	Bin        string `json:"bin,omitempty" yaml:"bin,omitempty"`
+	// The path to the `kubectl` binary. Defaults to "kubectl".
+	Bin string `json:"bin,omitempty" yaml:"bin,omitempty"`
+	// Override the default path to the kubeconfig file.
 	Kubeconfig string `json:"kubeconfig,omitempty" yaml:"kubeconfig,omitempty"`
-	Context    string `json:"context,omitempty" yaml:"context,omitempty"`
+	// Override the default kubeconfig context.
+	Context string `json:"context,omitempty" yaml:"context,omitempty"`
 
-	Namespaces        []string `json:"namespaces,omitempty" yaml:"namespaces,omitempty"`
-	NamespaceSelector string   `json:"namespaceSelector,omitempty" yaml:"namespaceSelector,omitempty"`
-	Types             []string `json:"types,omitempty" yaml:"types,omitempty"`
-	LabelSelector     string   `json:"labelSelector,omitempty" yaml:"labelSelector,omitempty"`
+	// An explicit list of namespaces to look for resources in.
+	Namespaces []string `json:"namespaces,omitempty" yaml:"namespaces,omitempty"`
+	// A label selector matching namespaces to look for resources in.
+	NamespaceSelector string `json:"namespaceSelector,omitempty" yaml:"namespaceSelector,omitempty"`
+	// The list of resource types to include. Defaults to "deployments,statefulsets,configmaps".
+	Types []string `json:"types,omitempty" yaml:"types,omitempty"`
+	// A label selector to limit which resources are included. Defaults to "" (match everything).
+	LabelSelector string `json:"labelSelector,omitempty" yaml:"labelSelector,omitempty"`
 }
 
+// Kustomize is used to expand kustomizations.
 type Kustomize struct {
 	yaml.ResourceMeta `json:",inline" yaml:",inline"`
 
+	// The Kustomize root to build.
 	Root string `json:"root" yaml:"root"`
 }
 
+// PasswordRecipe is used to configure random password strings for secrets.
 type PasswordRecipe struct {
-	Key         string `json:"key" yaml:"key"`
-	Length      *int   `json:"length,omitempty" yaml:"length,omitempty"`
-	NumDigits   *int   `json:"numDigits,omitempty" yaml:"numDigits,omitempty"`
-	NumSymbols  *int   `json:"numSymbols,omitempty" yaml:"numSymbols,omitempty"`
-	NoUpper     *bool  `json:"noUpper,omitempty" yaml:"noUpper,omitempty"`
-	AllowRepeat *bool  `json:"allowRepeat,omitempty" yaml:"allowRepeat,omitempty"`
+	// The key in the secret data field to use.
+	Key string `json:"key" yaml:"key"`
+	// The length of the password.
+	Length *int `json:"length,omitempty" yaml:"length,omitempty"`
+	// The number of digits to include in the password.
+	NumDigits *int `json:"numDigits,omitempty" yaml:"numDigits,omitempty"`
+	// The number of symbol characters to include in the password.
+	NumSymbols *int `json:"numSymbols,omitempty" yaml:"numSymbols,omitempty"`
+	// Flag restricting the use of uppercase characters.
+	NoUpper *bool `json:"noUpper,omitempty" yaml:"noUpper,omitempty"`
+	// Flag restricting repeating characters.
+	AllowRepeat *bool `json:"allowRepeat,omitempty" yaml:"allowRepeat,omitempty"`
 }
 
+// Secret is used to expand a Secret resource.
 type Secret struct {
 	yaml.ResourceMeta `json:",inline" yaml:",inline"`
 
+	// The type of secret to generate.
 	Type string `json:"type,omitempty" yaml:"type,omitempty"`
 
-	LiteralSources  []string         `json:"literals,omitempty" yaml:"literals,omitempty"`
-	FileSources     []string         `json:"files,omitempty" yaml:"files,omitempty"`
-	EnvSources      []string         `json:"envs,omitempty" yaml:"envs,omitempty"`
-	UUIDSources     []string         `json:"uuids,omitempty" yaml:"uuids,omitempty"`
-	ULIDSources     []string         `json:"ulids,omitempty" yaml:"ulids,omitempty"`
+	// A list of `key=value` pairs to include on the secret.
+	LiteralSources []string `json:"literals,omitempty" yaml:"literals,omitempty"`
+	// A list of files (or `key=filename` pairs) to include on the secret.
+	FileSources []string `json:"files,omitempty" yaml:"files,omitempty"`
+	// A list of .env files (files containing `key=value` pairs) to include on the secret.
+	EnvSources []string `json:"envs,omitempty" yaml:"envs,omitempty"`
+	// A list of keys to include randomly generated UUIDs for on the secret.
+	UUIDSources []string `json:"uuids,omitempty" yaml:"uuids,omitempty"`
+	// A list of keys to include randomly generated ULIDs for on the secret.
+	ULIDSources []string `json:"ulids,omitempty" yaml:"ulids,omitempty"`
+	// A list of password recipes to include random strings on the secret.
 	PasswordSources []PasswordRecipe `json:"passwords,omitempty" yaml:"passwords,omitempty"`
 
+	// Additional configuration for generating passwords.
 	PasswordOptions *password.GeneratorInput `json:"passwordOptions,omitempty" yaml:"passwordOptions,omitempty"`
 }
 
+// Git is used to expand full or partial Git repositories.
 type Git struct {
 	yaml.ResourceMeta `json:",inline" yaml:",inline"`
 
+	// The Git repository URL.
 	Repository string `json:"repo,omitempty" yaml:"repo,omitempty"`
-	Refspec    string `json:"refspec,omitempty" yaml:"refspec,omitempty"`
-	Context    string `json:"context,omitempty" yaml:"context,omitempty"`
+	// The refspec in the repository to checkout.
+	Refspec string `json:"refspec,omitempty" yaml:"refspec,omitempty"`
+	// The subdirectory context to limit the Git repository to.
+	Context string `json:"context,omitempty" yaml:"context,omitempty"`
 }
 
+// HTTP is used to expand HTTP resources.
 type HTTP struct {
 	yaml.ResourceMeta `json:",inline" yaml:",inline"`
 
+	// The HTTP(S) URL to fetch.
 	URL string `json:"url" yaml:"url"`
 }
 
+// File is used to expand local file system resources.
 type File struct {
 	yaml.ResourceMeta `json:",inline" yaml:",inline"`
 
+	// The file (or directory) name to read.
 	Name string `json:"name" yaml:"name"`
 }
