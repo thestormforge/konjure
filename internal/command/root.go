9d3c404c47954c05844d050a17b476945c083198
Jeremy Gustie <jeremy@stormforge.io>
2021-09-10 14:42:25 -0400

    Add missing output format

--- internal/command/root.go
+++ internal/command/root.go
@@ -78,7 +78,7 @@ func NewRootCommand(version, refspec, date string) *cobra.Command {
 	cmd.Flags().BoolVar(&f.KeepComments, "keep-comments", true, "retain YAML comments")
 	cmd.Flags().BoolVar(&f.Format, "format", false, "format output to Kubernetes conventions")
 	cmd.Flags().BoolVarP(&f.RecursiveDirectories, "recurse", "r", false, "recursively process directories")
-	cmd.Flags().StringVarP(&w.Format, "output", "o", "yaml", "set the output format (yaml, ndjson, env)")
+	cmd.Flags().StringVarP(&w.Format, "output", "o", "yaml", "set the output format (yaml, json, ndjson, env)")
 	cmd.Flags().BoolVar(&w.KeepReaderAnnotations, "keep-annotations", false, "retain annotations used for processing")
 	cmd.Flags().BoolVar(&w.Sort, "sort", false, "sort output prior to writing")
 
