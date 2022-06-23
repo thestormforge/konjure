c1a19aedbd82c253a650f09e976256228bacc9ee
Jeremy Gustie <jeremy@stormforge.io>
2022-06-23 16:10:46 -0400

    Add pipes.ErrorReader

--- pkg/pipes/pipes.go
+++ pkg/pipes/pipes.go
@@ -1,9 +1,17 @@
 package pipes
 
-import "sigs.k8s.io/kustomize/kyaml/yaml"
+import (
+	"sigs.k8s.io/kustomize/kyaml/yaml"
+)
 
 // ReaderFunc is an adapter to allow the use of ordinary functions as a kio.Reader.
 type ReaderFunc func() ([]*yaml.RNode, error)
 
 // Read evaluates the typed function.
-func (f ReaderFunc) Read() ([]*yaml.RNode, error) { return f() }
+func (r ReaderFunc) Read() ([]*yaml.RNode, error) { return r() }
+
+// ErrorReader is an adapter to allow the use of an error as a kio.Reader.
+type ErrorReader struct{ error }
+
+// Reader returns the wrapped failure.
+func (r ErrorReader) Read() ([]*yaml.RNode, error) { return nil, r }
