356d9f5e0c254a3282fd64f9355b9094caf962b7
Jeremy Gustie <jeremy@stormforge.io>
2022-08-09 14:35:32 -0400

    Add filters.SetPath and filters.SetValues

--- pkg/filters/fieldpath.go
+++ pkg/filters/fieldpath.go
@@ -17,10 +17,12 @@ limitations under the License.
 package filters
 
 import (
+	"strconv"
 	"strings"
 	"text/template"
 
 	"sigs.k8s.io/kustomize/kyaml/utils"
+	"sigs.k8s.io/kustomize/kyaml/yaml"
 )
 
 // FieldPath evaluates a path template using the supplied context and then
@@ -40,19 +42,97 @@ func FieldPath(p string, data map[string]string) ([]string, error) {
 		return nil, err
 	}
 
-	fieldPath, err := utils.SmarterPathSplitter(pathBuf.String(), "/"), nil
-	if err != nil {
-		return nil, err
+	return cleanPath(utils.SmarterPathSplitter(pathBuf.String(), "/")), nil
+}
+
+// SetPath returns a filter that sets the node value at the specified path.
+// Note that this path uses "." separators rather then "/".
+func SetPath(p string, v *yaml.RNode) yaml.Filter {
+	path := cleanPath(utils.SmarterPathSplitter(p, "."))
+
+	var fns []yaml.Filter
+	if yaml.IsMissingOrNull(v) {
+		if l := len(path) - 1; l == 0 {
+			fns = append(fns,
+				&yaml.FieldClearer{Name: path[l]},
+			)
+		} else {
+			fns = append(fns,
+				&yaml.PathGetter{Path: path[0:l]},
+				&yaml.FieldClearer{Name: path[l], IfEmpty: true},
+			)
+		}
+	} else {
+		fns = append(fns,
+			&yaml.PathGetter{Path: path, Create: v.YNode().Kind},
+			&yaml.FieldSetter{Value: v},
+		)
 	}
 
-	for {
-		switch {
-		case len(fieldPath) == 0:
-			return nil, nil
-		case fieldPath[0] == "":
-			fieldPath = fieldPath[1:]
-		default:
-			return fieldPath, nil
+	return yaml.FilterFunc(func(object *yaml.RNode) (*yaml.RNode, error) { return object.Pipe(fns...) })
+}
+
+// SetValues returns a filter that sets the supplied "path=value" specifications
+// onto incoming nodes. Use forceString to bypass tagging the node values.
+func SetValues(nameValue []string, forceString bool) yaml.Filter {
+	var fns []yaml.Filter
+	for _, spec := range nameValue {
+		fns = append(fns, SetPath(splitPathValue(spec, forceString)))
+	}
+
+	return yaml.FilterFunc(func(object *yaml.RNode) (*yaml.RNode, error) { return object.Pipe(fns...) })
+}
+
+// cleanPath removes all empty and white space path elements.
+func cleanPath(path []string) []string {
+	result := make([]string, 0, len(path))
+	for _, p := range path {
+		if p = strings.TrimSpace(p); p != "" {
+			result = append(result, p)
 		}
 	}
+	if len(result) > 0 {
+		return result
+	}
+	return nil
+}
+
+// splitPathValue splits a "path=value" into a path and an RNode.
+func splitPathValue(spec string, st bool) (string, *yaml.RNode) {
+	p := utils.SmarterPathSplitter(spec, ".")
+	for i := len(p) - 1; i >= 0; i-- {
+		path, value, ok := strings.Cut(p[i], "=")
+		if !ok {
+			continue
+		}
+
+		p[i] = path
+		path = strings.Join(p[0:i+1], ".")
+
+		p[i] = value
+		value = strings.Join(p[i:], ".")
+
+		node := yaml.NewStringRNode(value)
+		if st {
+			return path, node
+		}
+
+		switch strings.ToLower(value) {
+		case "true":
+			node.YNode().Tag = yaml.NodeTagBool
+		case "false":
+			node.YNode().Tag = yaml.NodeTagBool
+		case "null":
+			node.YNode().Tag = yaml.NodeTagNull
+			node.YNode().Value = ""
+		case "0":
+			node.YNode().Tag = yaml.NodeTagInt
+		default:
+			if _, err := strconv.ParseInt(value, 10, 64); err == nil && value[0] != '0' {
+				node.YNode().Tag = yaml.NodeTagInt
+			}
+		}
+		return path, node
+	}
+	return spec, nil
 }
--- pkg/filters/fieldpath_test.go
+++ pkg/filters/fieldpath_test.go
@@ -1,9 +1,12 @@
 package filters
 
 import (
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"sigs.k8s.io/kustomize/kyaml/kio"
+	"sigs.k8s.io/kustomize/kyaml/yaml"
 )
 
 func TestFieldPath(t *testing.T) {
@@ -44,6 +47,144 @@ func TestFieldPath(t *testing.T) {
 			if assert.NoError(t, err) {
 				assert.Equal(t, tc.expected, actual)
 			}
+		})
+	}
+}
+
+func TestSetValues(t *testing.T) {
+	cases := []struct {
+		desc        string
+		inputs      []*yaml.RNode
+		specs       []string
+		forceString bool
+		expected    string
+	}{
+		{
+			desc: "empty",
+		},
+		{
+			desc:   "simple name",
+			inputs: []*yaml.RNode{yaml.MustParse(`foo: bar`)},
+			specs:  []string{"abc=xyz"},
+			expected: `#
+foo: bar
+abc: xyz
+`,
+		},
+		{
+			desc:   "indexed name",
+			inputs: []*yaml.RNode{yaml.MustParse(`foo: bar`)},
+			specs:  []string{"a.b.[c=d].e=wxyz"},
+			expected: `#
+foo: bar
+a:
+  b:
+  - c: d
+    e: wxyz
+`,
+		},
+		{
+			desc: "no create null",
+			inputs: []*yaml.RNode{yaml.MustParse(`#
+foo: bar
+`)},
+			specs: []string{"abc=null"},
+			expected: `#
+foo: bar
+`,
+		},
+		{
+			desc: "unset",
+			inputs: []*yaml.RNode{yaml.MustParse(`#
+foo: bar
+abc: xyz
+`)},
+			specs: []string{"abc=null"},
+			expected: `#
+foo: bar
+`,
+		},
+		{
+			desc: "unset nested",
+			inputs: []*yaml.RNode{yaml.MustParse(`#
+foo: bar
+abc:
+  xyz: zyx
+`)},
+			specs: []string{"abc.xyz=null"},
+			expected: `#
+foo: bar
+abc: {}
+`,
+		},
+	}
+	for _, tc := range cases {
+		t.Run(tc.desc, func(t *testing.T) {
+			var buf strings.Builder
+			err := kio.Pipeline{
+				Inputs:  []kio.Reader{kio.ResourceNodeSlice(tc.inputs)},
+				Filters: []kio.Filter{kio.FilterAll(SetValues(tc.specs, tc.forceString))},
+				Outputs: []kio.Writer{kio.ByteWriter{Writer: &buf}},
+			}.Execute()
+			if assert.NoError(t, err, "Pipeline failed") {
+				assert.YAMLEq(t, tc.expected, buf.String())
+			}
+		})
+	}
+}
+
+func TestSplitPathValue(t *testing.T) {
+	cases := []struct {
+		desc          string
+		spec          string
+		expectedPath  string
+		expectedValue *yaml.RNode
+	}{
+		{
+			desc: "empty",
+		},
+		{
+			desc:          "simple name string value",
+			spec:          "name=value",
+			expectedPath:  "name",
+			expectedValue: yaml.NewStringRNode("value"),
+		},
+		{
+			desc:          "nested name string value",
+			spec:          "nom.name=value",
+			expectedPath:  "nom.name",
+			expectedValue: yaml.NewStringRNode("value"),
+		},
+		{
+			desc:          "indexed name string value",
+			spec:          "nom.name.[foo.bar=xyz].foobar=value",
+			expectedPath:  "nom.name.[foo.bar=xyz].foobar",
+			expectedValue: yaml.NewStringRNode("value"),
+		},
+		{
+			desc:          "string value with delimiter",
+			spec:          "name=Value. Such value.",
+			expectedPath:  "name",
+			expectedValue: yaml.NewStringRNode("Value. Such value."),
+		},
+		{
+			desc:          "empty string value",
+			spec:          "name=",
+			expectedPath:  "name",
+			expectedValue: yaml.NewStringRNode(""),
+		},
+		{
+			desc:          "null value",
+			spec:          "name=null",
+			expectedPath:  "name",
+			expectedValue: yaml.NewRNode(&yaml.Node{Kind: yaml.ScalarNode, Tag: yaml.NodeTagNull}),
+		},
+	}
+	for _, tc := range cases {
+		t.Run(tc.desc, func(t *testing.T) {
+			actualPath, actualValue := splitPathValue(tc.spec, false)
+			assert.Equal(t, tc.expectedPath, actualPath)
+			assert.Equal(t, tc.expectedValue, actualValue)
 		})
 	}
 }
