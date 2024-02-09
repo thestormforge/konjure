bd125acacb735af13a98844478c8e5ce603a79a7
Jeremy Gustie <jeremy@stormforge.io>
2024-02-09 11:00:04 -0500

    Try making the pipes.FileReader/Writer more configurable

--- pkg/pipes/file.go
+++ pkg/pipes/file.go
@@ -2,13 +2,40 @@ package pipes
 
 import (
 	"bytes"
+	"fmt"
 	"io/fs"
 	"os"
+	"path/filepath"
 
 	"sigs.k8s.io/kustomize/kyaml/kio"
 	"sigs.k8s.io/kustomize/kyaml/kio/kioutil"
 	"sigs.k8s.io/kustomize/kyaml/yaml"
 )
+
+type fileReadWriterOptions struct {
+	// The reader used to parse the file contents.
+	Reader *kio.ByteReader
+	// The writer used to encode the file contents.
+	Writer *kio.ByteWriter
+	// The default permissions are 0666.
+	WriteFilePerm os.FileMode
+	// If non-zero, create the parent directories with these permissions using MkdirAll.
+	MkdirAllPerm os.FileMode
+}
+
+// FileReadWriterOption represents a configuration option for the FileReader or FileWriter.
+type FileReadWriterOption func(opts *fileReadWriterOptions) error
+
+// FileWriterMkdirAll is an option that allows you to create all the parent directories on write.
+func FileWriterMkdirAll(perm os.FileMode) FileReadWriterOption {
+	return func(opts *fileReadWriterOptions) error {
+		if opts.Writer == nil {
+			return fmt.Errorf("mkdirAll requires a FileWriter")
+		}
+		opts.MkdirAllPerm = perm
+		return nil
+	}
+}
 
 // FileReader is a KIO reader that lazily loads a file.
 type FileReader struct {
@@ -16,9 +43,8 @@ type FileReader struct {
 	Name string
 	// The file system to use for resolving file contents (defaults to the OS reader).
 	FS fs.FS
-
-	// Temporary hack for configuring the byte reader.
-	HackHooks []func(*kio.ByteReader)
+	// Configuration options.
+	Options []FileReadWriterOption
 }
 
 // Read opens the configured file and reads the contents.
@@ -35,29 +61,30 @@ func (r *FileReader) Read() ([]*yaml.RNode, error) {
 		return nil, err
 	}
 
-	// Build a byte reader to process the content
-	br := &kio.ByteReader{
-		Reader: bytes.NewReader(data),
-		SetAnnotations: map[string]string{
-			kioutil.PathAnnotation: r.Name,
+	// Wrap the reader in an options struct for configuration
+	opts := fileReadWriterOptions{
+		Reader: &kio.ByteReader{
+			Reader: bytes.NewReader(data),
+			SetAnnotations: map[string]string{
+				kioutil.PathAnnotation: r.Name,
+			},
 		},
 	}
-
-	// TODO This is temporary while we figure out what is important
-	for _, h := range r.HackHooks {
-		h(br)
+	for _, opt := range r.Options {
+		if err := opt(&opts); err != nil {
+			return nil, err
+		}
 	}
 
-	return br.Read()
+	return opts.Reader.Read()
 }
 
 // FileWriter is a KIO writer that writes nodes to a file.
 type FileWriter struct {
 	// The file name to write.
 	Name string
-
-	// Temporary hack for configuring the byte writer.
-	HackHooks []func(*kio.ByteWriter)
+	// Configuration options.
+	Options []FileReadWriterOption
 }
 
 // Write overwrites the file with encoded nodes.
@@ -68,14 +95,32 @@ func (w *FileWriter) Write(nodes []*yaml.RNode) error {
 		Writer: &buf,
 	}
 
-	// TODO This is temporary while we figure out what is important
-	for _, h := range w.HackHooks {
-		h(bw)
+	// Wrap the reader in an options struct for configuration
+	opts := fileReadWriterOptions{
+		Writer: &kio.ByteWriter{
+			Writer: &buf,
+		},
+	}
+	for _, opt := range w.Options {
+		if err := opt(&opts); err != nil {
+			return err
+		}
 	}
 
 	if err := bw.Write(nodes); err != nil {
 		return err
 	}
 
-	return os.WriteFile(w.Name, buf.Bytes(), 0666)
+	if opts.MkdirAllPerm != 0 {
+		if err := os.MkdirAll(filepath.Dir(w.Name), opts.MkdirAllPerm); err != nil {
+			return err
+		}
+	}
+
+	perm := opts.WriteFilePerm
+	if perm == 0 {
+		perm = 0666
+	}
+
+	return os.WriteFile(w.Name, buf.Bytes(), perm)
 }
